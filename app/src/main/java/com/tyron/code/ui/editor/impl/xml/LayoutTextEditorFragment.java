d3c48f3929312131145499a5ad520b9ff4423077
masotomikara-dotcom <masotomikara@gmail.com>
2026-05-26 16:13:57 +0700

    Use custom absolutePath property matching project structure

--- app/src/main/java/com/tyron/code/ui/editor/impl/xml/LayoutTextEditorFragment.java
+++ app/src/main/java/com/tyron/code/ui/editor/impl/xml/LayoutTextEditorFragment.java
@@ -7,8 +7,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import java.io.File;
 
 public class LayoutTextEditorFragment extends Fragment {
+
+    public static LayoutTextEditorFragment newInstance(File file) {
+        LayoutTextEditorFragment fragment = new LayoutTextEditorFragment();
+        Bundle args = new Bundle();
+        if (file != null) {
+            args.putString("path", file.absolutePath);
+        }
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
